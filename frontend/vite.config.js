53e1ab2ac4ee6dc5a941ecf4793bd63f2666c4c8
rushibamb <rushibamb@gmail.com>
2025-08-02 01:05:14 +0530

    Move _redirects to root and configure Vite to copy it

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'build',
-  },
-  server: {
-    historyApiFallback: true, // for dev
-  }
-})
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '_redirects',
+          dest: '.',
+        },
+      ],
+    }),
+  ],
+});
