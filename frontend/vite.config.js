1651335c784484549570525acbc03e790fa0c68e
Your Name <your-email@example.com>
2025-08-09 11:25:37 +0530

    update config files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // important for relative paths in Vercel
+  base: '',
   build: {
     rollupOptions: {
-      input: '/index.html', // ensures proper routing
+      input: '/index.html',
     }
   }
-});
+})
